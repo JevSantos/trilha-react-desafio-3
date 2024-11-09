0b097d272cba8dcc1388f92db0975de1b841e083
Jeverson Santos <jeverson.apsantos@gmail.com>
2024-11-08 21:16:56 -0300

    Add cadastro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { Feed } from "./pages/feed";
+import React from 'react'
+import { useNavigate  } from "react-router-dom";
 
-import { Home } from './pages/home'
-import { Login } from './pages/login'
-import { GlobalStyle } from './styles/global';
+import logo from '../../assets/logo-dio.png';
 
-function App() {
+import { Button } from '../Button';
+
+import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
+
+const Header = ({autenticado}) => {
+
+  const navigate = useNavigate();
+
+  const handleClickSignUp = () => {
+      navigate('/signup')
+  }
+
   return (
-    <Router>
-     <GlobalStyle />
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/feed" element={<Feed />} />
-     </Routes >
-    </Router>
-  );
+    <Wrapper>
+      <Container>
+          <Row>
+            <img src={logo} alt="Logo da dio"/>
+            {autenticado ? (
+              <>
+               <BuscarInputContainer>
+                <Input placeholder='Buscar...'/>
+               </BuscarInputContainer>
+                <Menu>Live Code</Menu>
+                <Menu>Global</Menu>
+              </>
+            ) : null}
+          </Row>
+          <Row>
+              {autenticado ? (
+                <UserPicture src="https://avatars.githubusercontent.com/u/45184516?v=4"/>
+              ) : (
+              <>
+                <MenuRight href="/">Home</MenuRight>
+                <Button title="Entrar" />
+                <Button title="Cadastrar" onClick={handleClickSignUp}/>
+              </>)}
+          </Row>
+      </Container>
+    </Wrapper>
+  )
 }
 
-export default App;
+export { Header }
